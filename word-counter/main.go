ba1fe09271c94b290f0a178afcc5fd2d41c20719
Soy Carola <carolacaicedo1527@gmail.com>
2025-10-30 11:52:07 -0500

    Envio de agregacion de archvios en word-counter

---
+++ word-counter/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	countLines := false
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-l" {
+			countLines = true
+		}
+	}
+
+scanner := bufio.NewScanner(os.Stdin)
+total := 0
+
+
+	if countLines {
+		fmt.Println("Write your text (Counting Lines):")
+	} else {
+		fmt.Println("Write your text (Counting Words):")
+	}
+
+for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "Exit" {
+			break
+		}
+
+
+	if countLines {
+		total++
+
+	} else {
+		words := strings.Fields(line)
+		total += len(words)
+	}
+}
+
+
+	if countLines {
+
+		fmt.Printf("Lines: %d\n ", total)
+
+	} else {
+		fmt.Printf("Words: %d\n ", total)
+
+	}
+}
