080b2d71f89f12f729611f501e5adae31370786e
Soy Carola <carolacaicedo1527@gmail.com>
2025-11-25 16:13:06 -0500

    primera revision word-counter con flags

--- word-counter/main.go
+++ word-counter/main.go
@@ -2,52 +2,115 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func main() {
-
-	countLines := false
-
-	if len(os.Args) > 1 {
-		if os.Args[1] == "-l" {
-			countLines = true
-		}
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	total := 0
+// function that performs the counting process
+func countString(input string, countLines bool, countBytes bool) int {
 
 	if countLines {
-		fmt.Println("Write your text (Counting Lines):")
+
+		// Count the number of lines
+		scanner := bufio.NewScanner(strings.NewReader(input))
+		total := 0
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.EqualFold(strings.TrimSpace(line), "exit") {
+				break
+			}
+
+			total++
+		}
+
+		return total
+	} else if countBytes {
+		// Count the number of bytes
+		return len([]byte(input))
+
 	} else {
-		fmt.Println("Write your text (Counting Words):")
+
+		// Count the number of words
+		scanner := bufio.NewScanner(strings.NewReader(input))
+		total := 0
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.EqualFold(strings.TrimSpace(line), "exit") {
+
+				break
+			}
+
+			words := strings.Fields(line)
+			total += len(words)
+		}
+		return total
 	}
+
+}
+
+// main function
+func main() {
+
+	// Define flags
+	//countLines
+	countLines := flag.Bool("l", false, "Count lines")
+	//countBytes
+	countBytes := flag.Bool("b", false, "Count bytes")
+	//countWords (default)
+
+	//Show status before parsing
+	fmt.Printf("Before parsing - Lines: %t, Bytes: %t\n", *countLines, *countBytes)
+
+	// Parse the command line flags
+	flag.Parse()
+
+	// Show status after parsing
+	fmt.Printf("After parsing - Lines: %t, Bytes: %t\n", *countLines, *countBytes)
+
+	// Define the mode of counting
+	var mode string
+
+	if *countLines {
+		mode = "lines"
+	} else if *countBytes {
+		mode = "bytes"
+	} else {
+		mode = "words"
+	}
+
+	// Print the mode of counting
+	fmt.Printf("The program will count %s.\n", mode)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var input strings.Builder
+
+	fmt.Printf("Write your text (Counting %s):", mode)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if strings.ToLower(strings.TrimSpace(line)) == "exit" {
+		if strings.EqualFold(strings.TrimSpace(line), "exit") {
 			break
 		}
 
-		if countLines {
-			total++
+		input.WriteString(line + "\n")
 
-		} else {
-			words := strings.Fields(line)
-			total += len(words)
-		}
 	}
 
-	if countLines {
+	// Call the counting function
+	total := countString(input.String(), *countLines, *countBytes)
 
+	switch mode {
+	case "lines":
 		fmt.Printf("Lines: %d\n ", total)
-
-	} else {
+	case "bytes":
+		fmt.Printf("bytes: %d\n ", total)
+	default:
 		fmt.Printf("Words: %d\n ", total)
-
 	}
 }
---
+++ word-counter/word-counter_test.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"testing"
+)
+
+// function to count words with different conditions
+func TestCountString_Words(t *testing.T) {
+
+	//What is the input/data to be tested
+	tests := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+
+		{"TestSingleSentence", "A single sentence with a few words.", 7},
+
+		{"TestMultipleSentences", "This is a line of text.\n\t\t\tStart a line break here.", 11},
+
+		{"TestSingleWord", "hello", 1},
+
+		{"TestSingleComposedWord", "read-only", 1},
+
+		{"TestMultipleBreakLines", "This is a line of text.\n\nStart a line break here.", 11},
+
+		{"TestExitVarations", "that the program uses for output, Exit, exit, EXIT.", 9},
+
+		{"TestEmptyInput", "", 0},
+	}
+
+	//that yields the expected result
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := countString(tt.input, false, false)
+			if result != tt.expected {
+				t.Errorf("For input '%s': expected %d, got %d", tt.name, tt.expected, result)
+			}
+		})
+
+	}
+}
+
+// function to count lines with different conditions
+func TestCountString_Lines(t *testing.T) {
+	//What is the input/data to be tested
+	tests := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+		{"TestSingleLine", "Hello world, this is a single line of text.", 1},
+
+		{"TestMultipleLines", "This is a line of text.\nStart a line break here.", 2},
+
+		{"TestMultipleLinesWithBreaks", "This is a line of text.\n\nStart a line break here.", 3},
+
+		{"TestEmptyInput", "", 0},
+	}
+
+	//that yields the expected result
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := countString(tt.input, true, false)
+			if result != tt.expected {
+				t.Errorf("Test '%s' failed: expected '%d', got '%d'", tt.name, tt.expected, result)
+			}
+		})
+	}
+}
+
+// function to count bytes with different conditions
+func TestCountString_Bytes(t *testing.T) {
+	//What is the input/data to be tested
+	tests := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+		{"TestSingleLineBytes", "Hello world, this is a single line of text.", 43},
+
+		{"TestMultipleLinesBytes", "This is a line of text.\nStart a line break here.", 48},
+
+		{"TestMultipleLinesWithBreaksBytes", "This is a line of text.\n\nStart a line break here.", 49},
+
+		{"TestEmptyInputBytes", "", 0},
+	}
+
+	//that yields the expected result
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := countString(tt.input, false, true)
+			if result != tt.expected {
+				t.Errorf("Test '%s' failed: expected '%d', got '%d'", tt.name, tt.expected, result)
+			}
+		})
+	}
+}
+
+// Test aditional verufy priority countLines > countBytes > countWords
+func TestCountString_Priority(t *testing.T) {
+	//What is the input/data to be tested
+	input := "Line 1\nLine 2\nLine 3\n"
+
+	//When same flags are true, countLines should take priority
+	result := countString(input, true, true)
+	expected := 3
+
+	if result != expected {
+		t.Errorf("Test 'Priority' failed: expected '%d', got '%d'", expected, result)
+	}
+}
+
+// additional function, which accepts all types of outputs written in the program
+func TestCountString_Exit(t *testing.T) {
+	//What is the input/data to be tested, minus data (name)
+	test := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+		{"exit", "exit", 0},
+		{"Exit", "Exit", 0},
+		{"EXIT", "EXIT", 0},
+	}
+
+	//What is expected in the test outcome/result
+	for _, tt := range test {
+		t.Run(tt.input, func(t *testing.T) {
+			result := countString(tt.input, false, false)
+			if result != tt.expected {
+				t.Errorf("Test '%s' failed: expected '%d', got '%d'", tt.name, tt.expected, result)
+			}
+
+		})
+	}
+
+}
