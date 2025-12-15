c1dcc1aabba84bc55ee9c70766dfffc3e5a49a71
Soy Carola <carolacaicedo1527@gmail.com>
2025-12-15 15:39:43 -0500

    Exercise 5: Capture stderr and stdout separately

--- exec-command/main.go
+++ exec-command/main.go
@@ -84,18 +84,55 @@ package main
 // 	fmt.Printf("combined out: \n%s\n", string(out))
 // }
 
-//Exercise 4: Behind the scenes of CombinedOutput()
+// //Exercise 4: Behind the scenes of CombinedOutput()
 
-func (c *Cmd) CombinedOutput() ([]byte, error) {
-	if c.Stdout != nil {
-		return nil, errors.New("exec: Stdout already set")
+// func (c *Cmd) CombinedOutput() ([]byte, error) {
+// 	if c.Stdout != nil {
+// 		return nil, errors.New("exec: Stdout already set")
+// 	}
+// 	if c.Stderr != nil {
+// 		return nil, errors.New("exec: Stderr already set")
+// 	}
+// 	var b bytes.Buffer
+// 	c.Stdout = &b
+// 	c.Stderr = &b
+// 	err := c.Run()
+// 	return b.Bytes(), err
+// }
+
+// Exercise 5
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	// Command ls (list files) and -lah (long, all, KB, MB, not bytes)
+	cmd := exec.Command("ls", "-lah")
+	//runtime.GOOS is the var of system the program (windows, linux...)( programs to run on any system)
+	if runtime.GOOS == "windows" {
+		// if the system is windows, run the command talklist (A show all)(Q show owner)
+		cmd = exec.Command("cmd", "/C", "dir", "/Q", "/A")
 	}
-	if c.Stderr != nil {
-		return nil, errors.New("exec: Stderr already set")
+
+	// Vars for capture the output
+	// bytes.Buffer: var for binary data
+	var stdout, stderr bytes.Buffer
+	// Assign the output normal
+	cmd.Stdout = &stdout
+	// Assign the output error
+	cmd.Stderr = &stderr
+	//Run the command and wait for the end
+	err := cmd.Run()
+	if err != nil {
+		// if err is not nil, log the error and exit the program
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	var b bytes.Buffer
-	c.Stdout = &b
-	c.Stderr = &b
-	err := c.Run()
-	return b.Bytes(), err
+
+	// Convert the bytes.Buffer to string and show the output
+	outStr, errStr := stdout.String(), stderr.String()
+	fmt.Printf("out:\n%s\n err:\n%s\n", outStr, errStr)
 }
