8bd3d1cf9c6d094e7b15cffe989a39c9839900c1
Soy Carola <carolacaicedo1527@gmail.com>
2025-12-15 15:05:58 -0500

    Exercise 3: Running a command and capturing the output

--- exec-command/main.go
+++ exec-command/main.go
@@ -27,10 +27,39 @@ package main
 // 	}
 // }
 
-// Exercise 2: Running a command and showing output
+// // Exercise 2: Running a command and showing output
+// import (
+// 	"log"
+// 	"os"
+// 	"os/exec"
+// 	"runtime"
+// )
+
+// func main() {
+// 	// Command ls (list files) and -lah (long, all, KB, MB, not bytes)
+// 	cmd := exec.Command("ls", "-lah")
+// 	//runtime.GOOS is the var of system the program (windows, linux...)( programs to run on any system)
+// 	if runtime.GOOS == "windows" {
+// 		// if the system is windows, run the command talklist (A show all)(Q show owner)
+// 		cmd = exec.Command("cmd", "/C", "dir", "/Q", "/A")
+// 	}
+
+// 	// Redirige the exit command amd print in the console
+// 	cmd.Stdout = os.Stdout
+// 	// Same as cmd.Stdout but for mensages error
+// 	cmd.Stderr = os.Stderr
+// 	//Run the command and wait for the end
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		// if err is not nil, log the error and exit the program
+// 		log.Fatalf("cmd.Run() failed with %s\n", err)
+// 	}
+// }
+
+// Exercise 3: Running a command and capturing the output
 import (
+	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"runtime"
 )
@@ -40,18 +69,17 @@ func main() {
 	cmd := exec.Command("ls", "-lah")
 	//runtime.GOOS is the var of system the program (windows, linux...)( programs to run on any system)
 	if runtime.GOOS == "windows" {
-		// if the system is windows, run the command talklist (A show all)(Q show owner)
+		// if the system is windows, run the command "talklist" change for dir (A show all)(Q show owner)
 		cmd = exec.Command("cmd", "/C", "dir", "/Q", "/A")
 	}
 
-	// Redirige the exit command amd print in the console
-	cmd.Stdout = os.Stdout
-	// Same as cmd.Stdout but for mensages error
-	cmd.Stderr = os.Stderr
-	//Run the command and wait for the end
-	err := cmd.Run()
+	// Capture in out the output anr error in the same time (1 byte)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		// if err is not nil, log the error and exit the program
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
+
+	//Show the output
+	fmt.Printf("combined out: \n%s\n", string(out))
 }
