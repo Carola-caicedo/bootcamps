122d2c399bafd9e1fdfbcc9e290f714c50680baa
Soy Carola <carolacaicedo1527@gmail.com>
2025-12-15 15:33:36 -0500

    Exercise 4: Behind the scenes of CombinedOutput

--- exec-command/main.go
+++ exec-command/main.go
@@ -56,30 +56,46 @@ package main
 // 	}
 // }
 
-// Exercise 3: Running a command and capturing the output
-import (
-	"fmt"
-	"log"
-	"os/exec"
-	"runtime"
-)
+// // Exercise 3: Running a command and capturing the output
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os/exec"
+// 	"runtime"
+// )
 
-func main() {
-	// Command ls (list files) and -lah (long, all, KB, MB, not bytes)
-	cmd := exec.Command("ls", "-lah")
-	//runtime.GOOS is the var of system the program (windows, linux...)( programs to run on any system)
-	if runtime.GOOS == "windows" {
-		// if the system is windows, run the command "talklist" change for dir (A show all)(Q show owner)
-		cmd = exec.Command("cmd", "/C", "dir", "/Q", "/A")
+// func main() {
+// 	// Command ls (list files) and -lah (long, all, KB, MB, not bytes)
+// 	cmd := exec.Command("ls", "-lah")
+// 	//runtime.GOOS is the var of system the program (windows, linux...)( programs to run on any system)
+// 	if runtime.GOOS == "windows" {
+// 		// if the system is windows, run the command "talklist" change for dir (A show all)(Q show owner)
+// 		cmd = exec.Command("cmd", "/C", "dir", "/Q", "/A")
+// 	}
+
+// 	// Capture in out the output anr error in the same time (1 byte)
+// 	out, err := cmd.CombinedOutput()
+// 	if err != nil {
+// 		// if err is not nil, log the error and exit the program
+// 		log.Fatalf("cmd.Run() failed with %s\n", err)
+// 	}
+
+// 	//Show the output
+// 	fmt.Printf("combined out: \n%s\n", string(out))
+// }
+
+//Exercise 4: Behind the scenes of CombinedOutput()
+
+func (c *Cmd) CombinedOutput() ([]byte, error) {
+	if c.Stdout != nil {
+		return nil, errors.New("exec: Stdout already set")
 	}
-
-	// Capture in out the output anr error in the same time (1 byte)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		// if err is not nil, log the error and exit the program
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+	if c.Stderr != nil {
+		return nil, errors.New("exec: Stderr already set")
 	}
-
-	//Show the output
-	fmt.Printf("combined out: \n%s\n", string(out))
+	var b bytes.Buffer
+	c.Stdout = &b
+	c.Stderr = &b
+	err := c.Run()
+	return b.Bytes(), err
 }
