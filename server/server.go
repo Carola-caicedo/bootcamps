6b2321d00c5107979d8506d69d9a77cee0885639
Soy Carola <carolacaicedo1527@gmail.com>
2026-02-23 09:27:01 -0500

    Add set to Content-Type

--- server/server.go
+++ server/server.go
@@ -26,13 +26,14 @@ func errorReply(w http.ResponseWriter, _ *http.Request, status int, payload stri
 }
 
 func jsonReply(w http.ResponseWriter, _ *http.Request, status int, payload *todoResponse) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	data, err := json.Marshal(payload)
 	if err != nil {
 		errorReply(w, nil, http.StatusInternalServerError, "Error generated Response JSON")
 		return
-		
+
 	}
 
 	w.Write(data)
